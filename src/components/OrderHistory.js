efc38c05a911b013ecd95babed06f2b31c59e6ca
Yuriko Takamiya <yurikotakamiya@yurikonombp-2.mynetworksettings.com>
2022-03-31 17:32:50 -0400

    seaching function modified

--- src/components/OrderHistory.js
+++ src/components/OrderHistory.js
@@ -1,43 +1,20 @@
 import React from 'react'
 import { PropTypes } from 'prop-types'
+import ReactFlexyTable from "react-flexy-table"
+import "react-flexy-table/dist/index.css"
 
 const OrderHistory = props => {
     const { histories } = props
+    
     return (
         <div className='history-table'>
-            <h2>Order History</h2>
-            <div className='history-header'>
-                <h2>Trading Pair</h2>
-                <h2>Order Side</h2>
-                <h2>Order State</h2>
-                <h2>Strategy</h2>
-                <h2>Price</h2>
-                <h2>Size</h2>
-                <h2>Created at</h2>
-                <h2>Updated at</h2>
-            </div>
-            {
-                histories.map((history, idx)=> {
-                    return (
-                        <div key={idx} className='history-row'>
-                            <h3>{history.trading_pair}</h3>
-                            <h3>{history.order_side}</h3>
-                            <h3>{history.order_state}</h3>
-                            <h3>{history.strategy}</h3>
-                            <h3>{history.order_price}</h3>
-                            <h3>{history.order_size}</h3>                            
-                            <h3>{history.update_time}</h3>
-                            <h3>{history.create_time}</h3>
-                        </div>
-                    )
-                })
-            }
-            
-            </div>
+            <ReactFlexyTable data={histories} filterable/>    
+        </div>
     )
 }
 
 export default OrderHistory
+
 OrderHistory.propTypes = {
     histories: PropTypes.array,
 }
--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,36 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import OrderHistory from './OrderHistory'
-// import axiosWithAuth from '../utils/axiosWithAuth'
 import axios from 'axios'
 
 const Orders = () => {
-
-    const [ state, setState ] = useState({
-        strategy_id: '',
-        trading_pair_id: '',
-        order_side_id: '',
-        order_type_id: ''
-    })
     const [ histories, setHistory ] = useState([])
-    const handleChange = e => {
-        setState({
-            ...state,
-            [e.target.name]: e.target.value
-        })
-    }
-    const user_id = localStorage.getItem('id')
-    const handleSubmit = e => {
-        e.preventDefault()
-        let stateToSend = {}
-        for (let key in state) {
-            if (state[key] != '') {
-                stateToSend[key] = state[key]
-            }
-        }
-        stateToSend['user_id'] = user_id
-        console.log(stateToSend)
-        
-    }
     
     useEffect(() => {
         const user_id = localStorage.getItem('id')
@@ -40,74 +13,22 @@ const Orders = () => {
                 sid: sid,
             }
         })
-            .then(res => {
-                console.log(res.data)
-                setHistory(res.data)
+        .then(res => {
+            res.data.map(h => {
+                let create = new Date(h['create_time'])
+                let update = new Date(h['update_time'])
+                h['create_time'] = create.toLocaleString()
+                h['update_time'] = update.toLocaleString() 
+                return h
             })
-            .catch(e => console.log(e))
+            setHistory(res.data)
+        })
+        .catch(e => console.log(e))
     }, [])
+
     return (
         <div className='orders'>
-            <div className='search-bar'>
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        Strategy:
-                        <select
-                        name='strategy_id'
-                        type='text'
-                        value={state.strategy}
-                        onChange={handleChange}
-                        >
-                            <option value=''>-- Select Strategy --</option>
-                            <option value='1'>example1</option>                            
-                        </select>
-                    </label>
-                    <label>
-                        Trading Pair:
-                        <select
-                        name='trading_pair_id'
-                        type='text'
-                        value={state.trading_pair}
-                        onChange={handleChange}
-                        >
-                            <option value=''>-- Select Trading Pair --</option>
-                            <option value='1'>BTCUSDT</option>
-                            <option value='2'>ETHUSDT</option>
-                            <option value='3'>BTCPERP</option>
-                            <option value='4'>ETHPERP</option>
-                        </select>
-                    </label>
-                    <label>
-                        Order Side:
-                        <select
-                        name='order_side_id'
-                        type='text'
-                        value={state.order_side_id}
-                        onChange={handleChange}
-                        >
-                            <option value=''>-- Select Order Side --</option>
-                            <option value='1'>Buy</option>
-                            <option value='2'>Sell</option>
-                            <option value='3'>Sell Short</option>                            
-                        </select>
-                    </label>
-                    <label>
-                        Order Side:
-                        <select
-                        name='order_type_id'
-                        type='text'
-                        value={state.order_type_id}
-                        onChange={handleChange}
-                        >
-                            <option value=''>-- Select Order Type --</option>
-                            <option value='1'>Day Order</option>
-                            <option value='2'>IOC(Immediate-or-cancel) Order</option>
-                            <option value='3'>FOK(Fill-or-kill) Order</option>
-                        </select>
-                    </label>
-                    <button>Search History</button>
-                </form>
-            </div>
+            <h1>Your Orders</h1>
             <OrderHistory histories={histories} />
         </div>
     )
